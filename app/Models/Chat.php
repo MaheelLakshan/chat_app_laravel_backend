f01f9e88fc3136bfc05cd2b093a7e9015c5544f2
MaheelLakshan <MaheelLakshanra@gmail.com>
2024-01-14 21:17:15 +0530

    Chat Controller methods defined

--- app/Models/Chat.php
+++ app/Models/Chat.php
@@ -22,8 +22,15 @@ class Chat extends Model
         return $this->hasMany(ChatMessage::class, 'chat_id');
     }
 
-    public function lastMessages()
+    public function lastMessage()
     {
         return $this->hasOne(ChatMessage::class, 'chat_id')->latest('updated_at');
+    }
+
+    public function scopeHasParticipant($query, int $userId)
+    {
+        return $query->whereHas('participants', function ($q) use ($userId) {
+            $q->where('user_id', $userId);
+        });
     }
 }
